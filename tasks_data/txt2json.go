12157288632d3408a9ba931ddebd8ea6c9d02575
Arseniy Kucher <beeffallo@gmail.com>
2019-10-07 01:41:16 +0300

    reflect powered txt2json

--- tasks_data/txt2json.go
+++ tasks_data/txt2json.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"reflect"
@@ -18,27 +19,55 @@ type task struct {
 	Question    string   `doc:"q"`
 	Explanation string   `doc:"exp"`
 	Answer      []string `col:"ans"`
+	// Hello       string   `col:"hello"`
 }
 
-func parse(data string) []task {
-	tasks := []task{}
+func parse(data string, v interface{}) error {
+	valPtr := reflect.ValueOf(v)
+	if valPtr.Kind() != reflect.Ptr || valPtr.IsNil() {
+		return errors.New("non-pointer passed to Parse")
+	}
+
+	val := valPtr.Elem()
+	if val.Kind() != reflect.Slice {
+		return errors.New("non-slice under pointer passed to Parse")
+	}
+
 	lines := strings.Split(data, "\n")
 	for i := 0; i < len(lines); i++ {
 		line := lines[i]
-		tasks = append(tasks, parseLine(line))
+		// print(val.Kind())
+		// print(val.Type().Elem().NumField())
+		elemType := val.Type().Elem()
+		// println(elemType.Kind())
+		sub := reflect.New(elemType).Elem()
+		// println(sub.Elem().NumField())
+		// println(sub.Kind())
+		// println(sub.Elem().Kind())
+		// sub.ty
+		if err := parseLine(line, sub); err != nil {
+			panic(err)
+		}
+		val.Set(reflect.Append(val, sub))
 	}
-	return tasks
+
+	return nil
 }
 
-func parseLine(line string) task {
-	task := task{}
-	fields := reflect.TypeOf(task)
-	values := reflect.ValueOf(&task)
+func parseLine(line string, v reflect.Value) error {
+	// fields := reflect.TypeOf(v)
+	// print(fields.NumField())
+	// values := reflect.ValueOf(&v)
+	// print(v.Kind())
+	fields := v.Type()
+	// print(fields.NumField())
+	values := v
+
 	for i := 0; i < fields.NumField(); i++ {
 		tag := fields.Field(i).Tag
 		key := strings.Split(string(tag), ":")[0]
 		indexer := tag.Get(key)
-		value := values.Elem().Field(i)
+		value := values.Field(i)
 		switch key {
 		case "doc":
 			doc := cutDoc(line, indexer)
@@ -51,7 +80,7 @@ func parseLine(line string) task {
 		}
 	}
 	// fmt.Println(task)
-	return task
+	return nil
 }
 
 // q: "Black", ans: ["white", "White"], exp: "Opposite"
@@ -96,15 +125,36 @@ func main() {
 	data, err := ioutil.ReadFile("tasks.txt")
 	check(err)
 
-	tasks := parse(string(data))
-	// fmt.Println(tasks)
+	var tasks []task
+	// var tasks []task
+	if err := parse(string(data), &tasks); err != nil {
+		panic(err)
+	}
+	fmt.Println(tasks)
 
 	b, err := json.Marshal(&tasks)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(string(b))
+	check(err)
+	// fmt.Println(string(b))
+
+	bp, err := json.MarshalIndent(&tasks, "", "\t")
+	// fmt.Println(string(bp))
 
 	err = ioutil.WriteFile("parsed.json", b, 0)
+	check(err)
+
+	err = ioutil.WriteFile("pretty_parsed.json", bp, 0)
+	check(err)
+
+	// var sli []interface{}
+	// sl := []int{1, 2, 3}
+	// for i := 0; i < len(sl); i++ {
+	// 	sli = append(sli, sl[i])
+	// }
+	// fmt.Println(sli)
+
+	// byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
+	// var dat map[string]interface{}
+	// if err := json.Unmarshal(byt, &dat); err != nil {
+	// 	panic(err)
+	// }
 }
