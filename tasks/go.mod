41e1383c7a41496328371478c994eaa62962eb14
Sergey Cheremshinsky <sergey.cheremshinsky@gmail.com>
2019-10-11 23:45:49 +0300

    show f

---
+++ tasks/go.mod
@@ -0,0 +1,5 @@
+module github.com/orsenkucher/crystalpuzzle/tasks
+
+go 1.13
+
+require cloud.google.com/go/firestore v1.0.0
--- tasks/tasks.go
+++ tasks/tasks.go
@@ -2,11 +2,41 @@ package tasks
 
 // gcloud functions deploy HelloGet --runtime go111 --trigger-http
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+
+	"cloud.google.com/go/firestore"
 )
+
+type Task struct {
+	Question    string `firestore:"question"`
+	Explanation string `firestore:"explanation"`
+	Answer      string `firestore:"answer"`
+}
+
+func show(ctx context.Context, doc *firestore.DocumentRef, w http.ResponseWriter) {
+	docsnap, _ := doc.Get(ctx)
+	dataMap := docsnap.Data()
+	fmt.Fprint(w, dataMap, "\n")
+}
 
 // HelloGet is an HTTP Cloud Function.
 func HelloGet(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, "crystal-factory")
+	if err != nil {
+		log.Fatalf("create client: %v", err)
+	}
+
+	task := client.Doc("Tasks/listtest")
+	task.Set(ctx, Task{
+		Question:    "Question?",
+		Explanation: "explanation~",
+		Answer:      "answer!",
+	})
+
+	show(ctx, task, w)
 	fmt.Fprint(w, "Hello, World!")
 }
