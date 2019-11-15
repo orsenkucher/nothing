5c21ed4c54acbcfd5706eabbf7c8f18691d19840
Arseniy Kucher <beeffallo@gmail.com>
2019-11-15 23:19:19 +0200

    lerps

--- frontend/nothing/lib/ui/dev/cards.dart
+++ frontend/nothing/lib/ui/dev/cards.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -38,7 +39,8 @@ class _CardsState extends State<Cards> with SingleTickerProviderStateMixin {
   AnimationController _controller;
   int _index = 0;
   List<Size> _sizes = List<Size>();
-  List<Offset> _offsets = List<Offset>();
+  List<Alignment> _aligns = List<Alignment>();
+  double _normedOffset = 0.5;
 
   @override
   void initState() {
@@ -49,15 +51,24 @@ class _CardsState extends State<Cards> with SingleTickerProviderStateMixin {
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
+    print('didChangeDependencies');
+    _sizes.clear();
+    _aligns.clear();
     var size = MediaQuery.of(context).size;
+    var gap = Size(
+      size.width * (1 - widget._widthFactor) / 2,
+      size.height * (1 - widget._heightFactor) / 2,
+    );
     for (int i = 0; i < widget._stackCount + 1; i++) {
       _sizes.add(Size(
-        size.width * widget._widthFactor * (1 - i * 0.04),
-        size.height * widget._heightFactor * (1 - i * 0.04),
+        size.width * widget._widthFactor -
+            (gap.width / widget._stackCount) * i * 2.5,
+        size.height * widget._heightFactor -
+            (gap.height / widget._stackCount) * i,
       ));
-      _offsets.add(Offset(
-        (size.width - _sizes[i].width) / 2,
-        (size.height - _sizes[i].height) / 2 + 20 * i,
+      _aligns.add(Alignment(
+        0.0,
+        (0.7 / (widget._stackCount - 1)) * i,
       ));
     }
   }
@@ -80,16 +91,17 @@ class _CardsState extends State<Cards> with SingleTickerProviderStateMixin {
 
   Widget _gestureDetector(BuildContext context) {
     final mainCardSize = _sizes[0];
-    final mainOffset = _offsets[0];
-    return Positioned(
-      left: mainOffset.dx,
-      top: mainOffset.dy,
+    final mainAlign = _aligns[0];
+    return Align(
+      alignment: mainAlign,
       child: SizedBox(
         width: mainCardSize.width,
         height: mainCardSize.height,
         child: GestureDetector(
           onHorizontalDragUpdate: (upd) {
-            print(upd.delta);
+            setState(() {
+              _normedOffset += upd.delta.dx / MediaQuery.of(context).size.width;
+            });
           },
           child: Container(color: Colors.green.withAlpha(50)),
         ),
@@ -100,18 +112,41 @@ class _CardsState extends State<Cards> with SingleTickerProviderStateMixin {
   List<Widget> _buildCards(BuildContext context) {
     var start = min(widget._stackCount + _index, widget._totalCount) - 1;
     var end = _index;
-    return [for (int i = start; i >= end; i--) _buildCard(context, i)];
+    return [
+      // build transparent card here
+      for (int i = start; i >= end + 1; i--) _buildCard(context, i),
+      _buildFrontCard(context, end),
+    ];
+  }
+
+  Widget _buildFrontCard(BuildContext context, int idx) {
+    var size = _sizes[idx];
+    var align = _aligns[idx];
+    return Align(
+      alignment: align,
+      child: SizedBox(
+        width: size.width,
+        height: size.height,
+        child: widget._cardBuilder(
+          context,
+          widget._contentBuilder(context, idx),
+          idx == _index,
+          idx - _index,
+        ),
+      ),
+    );
   }
 
   Widget _buildCard(BuildContext context, int idx) {
     final curSize = _sizes[idx];
-    final curOffset = _offsets[idx];
-    return Positioned(
-      left: curOffset.dx,
-      top: curOffset.dy,
+    final curAlign = _aligns[idx];
+    final nextSize = _sizes[idx - 1];
+    final nextAlign = _aligns[idx - 1];
+    return Align(
+      alignment: Alignment.lerp(curAlign, nextAlign, _normedOffset),
       child: SizedBox(
-        width: curSize.width,
-        height: curSize.height,
+        width: lerpDouble(curSize.width, nextSize.width, _normedOffset),
+        height: lerpDouble(curSize.height, nextSize.height, _normedOffset),
         child: widget._cardBuilder(
           context,
           widget._contentBuilder(context, idx),
