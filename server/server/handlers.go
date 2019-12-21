2bf0ffb7cb78978094acd70105ce395e7c246a41
Arseniy Kucher <beeffallo@gmail.com>
2019-12-21 21:15:18 +0200

    debug

--- server/server/handlers.go
+++ server/server/handlers.go
@@ -8,8 +8,8 @@ import (
 	"net/http"
 )
 
-// Rqs : request
-type Rqs struct {
+// QRequest represents questions request bundled with previous answers
+type QRequest struct {
 	N       int
 	UserID  string
 	Answers map[int]bool
@@ -22,11 +22,12 @@ func (s *Server) GetQues(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 	defer r.Body.Close()
-	var req Rqs
+	var req QRequest
 	err = json.Unmarshal(body, &req)
 	if err != nil {
 		log.Println(err)
 	}
+	log.Println(req)
 	s.ReceiveAns(req.UserID, req.Answers)
 	ques := s.GiveQuestions(req.UserID, req.N)
 	quesj, err := json.Marshal(&ques)
