3ab1af8aad5fb139a359c119804acd4bf9f33fc9
Sergey Cheremshinsky <sergey.cheremshinsky@gmail.com>
2019-12-23 14:36:24 +0200

    data server

---
+++ server/data/sendquestions.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/orsenkucher/nothing/server/server"
+)
+
+func main() {
+	dat, _ := ioutil.ReadFile("questions.txt")
+	str := string(dat)
+	lines := strings.Split(str, "\n")
+	serv := server.Server{}
+	serv.StartUp()
+	for _, l := range lines {
+		var question server.Question
+		strs := strings.Split(l, "|")
+		if len(strs) < 3 {
+			fmt.Println("ALARM We miss ", l)
+			continue
+		}
+		question.Question = strs[0]
+		question.Left = strs[1]
+		question.Right = strs[2]
+		fmt.Println(question)
+		serv.AddQuestion(question)
+	}
+}
