0da0937daee01d2eb866fa76d125f4af15940fe5
Arseniy Kucher <beeffallo@gmail.com>
2019-12-28 18:29:23 +0200

    statsbar colors

--- frontend/nothing/lib/ui/statsbar.dart
+++ frontend/nothing/lib/ui/statsbar.dart
@@ -10,26 +10,42 @@ class StatsBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(16),
-      child: LayoutBuilder(
-        builder: (context, constraints) => Stack(
-          children: [
-            Container(
-              color: Color(0xff427b58), //0xffd65d0e
-              width: constraints.maxWidth,
-              height: height,
-            ),
-            AnimatedContainer(
-              duration: Duration(milliseconds: 500),
-              curve: Curves.easeOutCubic,
-              color: Color(0xffaf3a03),
-              width: constraints.maxWidth * value,
-              height: height,
-            ),
-          ],
+    return Stack(children: [
+      ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: LayoutBuilder(
+          builder: (context, constraints) => Stack(
+            children: [
+              Container(
+                color: Color(0xffd5c4a1), //0xffd65d0e //0xff427b58
+                width: constraints.maxWidth,
+                height: height,
+              ),
+              AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                curve: Curves.easeOutCubic,
+                color: Color(0xff665c54),
+                width: constraints.maxWidth * value,
+                height: height,
+              ),
+            ],
+          ),
         ),
       ),
-    );
+      LayoutBuilder(
+        builder: (context, constraints) => Container(
+          // color: Color(0xff32302f),
+          width: constraints.maxWidth,
+          height: height,
+          // decoration: BoxDecoration(
+          //   borderRadius: BorderRadius.circular(16),
+          //   border: Border.all(
+          //     color: Color(0xff32302f),
+          //     width: 4,
+          //   ),
+          // ),
+        ),
+      )
+    ]);
   }
 }
