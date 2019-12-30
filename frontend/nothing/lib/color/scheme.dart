e875ee94456c5d1571d83890e594667f16c78f8e
Arseniy Kucher <beeffallo@gmail.com>
2019-12-30 14:44:37 +0200

    color scheme

---
+++ frontend/nothing/lib/color/scheme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+extension NothingScheme on ColorScheme {
+  Color get background => const Color(0xff1d2021);
+  Color get card => const Color(0xff32302f);
+  Color get cardborder => const Color(0xffa89984);
+  Color get shadow => Colors.black.withAlpha(60);
+  Color get textbase => const Color(0xff83a598);
+  Color get textleft => const Color(0xffd3869b);
+  Color get textright => const Color(0xff8ec07c);
+  Color get question => const Color(0xffd5c4a1);
+  Color get sliderleft => const Color(0xff665c54);
+  Color get sliderright => const Color(0xffd5c4a1);
+  Color get slidertextleft => const Color(0xff1d2021);
+  Color get slidertextright => const Color(0xfffbf1c7);
+}
