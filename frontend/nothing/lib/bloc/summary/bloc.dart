d216486fc7543f28f2b23387e5fb79483695f599
Arseniy Kucher <beeffallo@gmail.com>
2020-01-17 23:48:17 +0200

    wip-3: fast-swipe protection

--- frontend/nothing/lib/bloc/summary/bloc.dart
+++ frontend/nothing/lib/bloc/summary/bloc.dart
@@ -18,43 +18,56 @@ class SummaryBloc extends Bloc<SummaryEvent, Summary> {
     Stream<Summary> Function(SummaryEvent) next,
   ) {
     return super.transformEvents(
-      events
-          // .asBroadcastStream()
-          .doOnEach(
-        (x) => print("**EVENTS $x"),
-      )
-          .split(
-        (e) => e is NewAnswer,
-        (yes) {
-          // yes = yes.asBroadcastStream();
-          final window = yes
-              .doOnEach((_) => print("**Bounce IN"))
-              .debounceTime(const Duration(milliseconds: 330))
-              .doOnEach((_) => print("**Bounce OUT"))
-                ..listen((x) => print("YOYOYO $x"));
-          // .pipe(streamConsumer);
-          return yes
-              .doOnEach((e) => print("**YES $e"))
-              .buffer(
-                // yes.debounceTime(const Duration(milliseconds: 150)),
-                window,
-              )
-              // .where((xs) => xs.length > 0)
-              .where(_where)
-              .map((xs) => xs.first)
-              .doOnEach(
-                (x) => print('--- YES: summary -> ${x.value}'),
-              );
-        },
-        (no) => no.doOnEach((x) => print(" --- NO: ${x.value}")),
-        // ..listen(print),
-      ),
+      // events
+      //     .doOnEach(
+      //   (x) => print("**EVENTS $x"),
+      // )
+      //     .split(
+      //   (e) => e is NewAnswer,
+      //   (yes) {
+      //     final window = yes
+      //         .doOnEach((_) => print("**Bounce IN"))
+      //         .debounceTime(const Duration(milliseconds: 330))
+      //         .doOnEach((_) => print("**Bounce OUT"))
+      //           ..listen((x) => print("YOYOYO $x"));
+      //     return yes
+      //         .doOnEach((e) => print("**YES $e"))
+      //         .buffer(
+      //           window,
+      //         )
+      //         .where((xs) => xs.length > 0)
+      //         // .where(_where)
+      //         .map((xs) => xs.first)
+      //         .doOnEach(
+      //           (x) => print('--- YES: summary -> ${x.value}'),
+      //         );
+      //   },
+      //   (no) => no.doOnEach((x) => print(" --- NO: ${x.value}")),
+      // ),
+      // events,
+      // (Stream<SummaryEvent> events) {
+      //   return events;
+      // }(events),
+      () {
+        // final events = events
+        final local = events.publishValue();
+        // final local = events.publish().autoConnect();
+        final yesline = local
+            .where((x) => x is NewAnswer)
+            .buffer(local.debounceTime(const Duration(seconds: 1)))
+            .where((xs) => xs.length > 0)
+            .map((xs) => xs.first)
+            .doOnEach((x) => print('YES: $x'));
+        final noline = local
+            .where((x) => !(x is NewAnswer))
+            .doOnEach((x) => print('NO: $x'));
+        final merged = yesline.mergeWith([noline]);
+        // local.conn
+        local.connect();
+        return merged;
+      }(),
       next,
     );
-  }
-
-  bool _where(List<SummaryEvent> l) {
-    return l.length > 0;
   }
 
   @override
@@ -73,18 +86,18 @@ class SummaryBloc extends Bloc<SummaryEvent, Summary> {
   }
 }
 
-extension Split<T> on Stream<T> {
-  Stream<T> split(
-    bool condition(T event),
-    Stream<T> yes(Stream<T> stream),
-    Stream<T> no(Stream<T> stream),
-  ) {
-    final broadcast = this.asBroadcastStream();
-    return Rx.merge([
-      broadcast.where(condition).chain(yes),
-      broadcast.where((x) => !condition(x)).chain(no),
-    ]);
-  }
+// extension Split<T> on Stream<T> {
+//   Stream<T> split(
+//     bool condition(T event),
+//     Stream<T> yes(Stream<T> stream),
+//     Stream<T> no(Stream<T> stream),
+//   ) {
+//     final broadcast = this.asBroadcastStream();
+//     return Rx.merge([
+//       broadcast.where(condition).chain(yes),
+//       broadcast.where((x) => !condition(x)).chain(no),
+//     ]);
+//   }
 
-  Stream<T> chain(Stream<T> logic(Stream<T> stream)) => logic(this);
-}
+//   Stream<T> chain(Stream<T> logic(Stream<T> stream)) => logic(this);
+// }
