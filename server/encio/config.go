56a6088441b2eb2a21d352dc77dc8b41ebf4b56b
Arseniy Kucher <beeffallo@gmail.com>
2020-01-31 17:59:50 +0200

    gorm.DB

--- server/encio/config.go
+++ server/encio/config.go
@@ -21,7 +21,7 @@ func (key EncIO) GetConfig(file string) (Config, error) {
 func (key EncIO) reloadConfig(file string) error {
 	if bytes, err := key.ReadFile(file); err == nil {
 		if err := json.Unmarshal(bytes, &cfg); err == nil {
-			log.Printf("encio: Config reload %v", cfg)
+			log.Printf("encio: Config reload -> %v", cfg)
 		} else {
 			return fmt.Errorf("encio: Your '%s' is present, but your key might be wrong.\nerr: %w", file, err)
 		}
