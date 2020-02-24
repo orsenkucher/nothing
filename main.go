1378ac167f8a9b399697b64bde3d60a712490c9c
Sergey Cheremshinsky <sergey.cheremshinsky@gmail.com>
2020-02-24 21:05:04 +0200

    server log

--- main.go
+++ main.go
@@ -42,11 +42,18 @@ func main() {
 	db := NewDB(key, cfg)
 	defer db.Close()
 
+	// anss := []server.User{}
+
+	// db.Find(&anss)
+	// fmt.Print(anss)
+	// fmt.Println("cleared")
+	// fmt.Scanln()
+
 	s := server.StartUp(db)
 	//s.ClearBase()
 	s.ShowStatus()
-	// fmt.Println("cleared")
-	// fmt.Scanln()
+	//fmt.Println("cleared")
+	fmt.Scanln()
 
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
