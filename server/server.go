5eb67c08cebfbe4c639d8b188fa124be1f67e42a
Sergey Cheremshinsky <sergey.cheremshinsky@gmail.com>
2020-02-27 18:27:51 +0200

    seems to be working:)

--- server/server.go
+++ server/server.go
@@ -238,9 +238,6 @@ func (s *Server) UpdateData() {
 	data, _ := ioutil.ReadFile("./data/questions.txt")
 	lines := strings.Split(string(data), "\n")
 	for _, line := range lines {
-		if len(s.Questions) > 10 {
-			break
-		}
 		fmt.Println(line)
 		parts := strings.Split(line, "|")
 		if len(parts) < 3 {
