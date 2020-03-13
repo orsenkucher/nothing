6c364b1ad4416bea89d36d0c8049b3d220726a01
Sergey Cheremshinsky <sergey.cheremshinsky@gmail.com>
2020-03-14 01:55:36 +0200

    secNorm = 60

--- server/mmr.go
+++ server/mmr.go
@@ -8,7 +8,7 @@ var max int = 10000
 var min int = 0
 var maxUserDif float64 = 1500
 var maxQuestionDif float64 = 100
-var secNorm float64 = 40
+var secNorm float64 = 60
 var smoothness float64 = 4
 var winRate float64 = 0.6
 
