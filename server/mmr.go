c64fa1bc5f140e04f5aca98b3d12a4ec168eb29a
Sergey Cheremshinsky <sergey.cheremshinsky@gmail.com>
2020-03-14 00:23:57 +0200

    changed mmr

--- server/mmr.go
+++ server/mmr.go
@@ -7,7 +7,7 @@ import (
 var max int = 10000
 var min int = 0
 var maxUserDif float64 = 1500
-var maxQuestionDif float64 = 50
+var maxQuestionDif float64 = 100
 var secNorm float64 = 40
 var smoothness float64 = 4
 var winRate float64 = 0.6
