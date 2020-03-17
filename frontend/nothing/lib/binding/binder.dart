906fa25d291c7ef9c609e2f648c9b496cec695b7
Arseniy Kucher <beeffallo@gmail.com>
2020-03-17 17:27:23 +0200

    binder conditions

--- frontend/nothing/lib/binding/binder.dart
+++ frontend/nothing/lib/binding/binder.dart
@@ -7,15 +7,21 @@ mixin BlocBinderSingleChildWidget on SingleChildWidget {}
 typedef BlocBinderListener<S1, B2> = void Function(
     BuildContext context, S1 state, B2 bloc);
 
+typedef BlocBinderCondition<S> = bool Function(S previous, S current);
+
 /// [BlocBinder] is helpful to bind or cross-bind two BloCs
 class BlocBinder<B1 extends Bloc<dynamic, S1>, S1, B2 extends Bloc<dynamic, S2>,
     S2> extends SingleChildStatelessWidget with BlocBinderSingleChildWidget {
   final BlocBinderListener<S1, B2> direct;
   final BlocBinderListener<S2, B1> reverse;
+  final BlocBinderCondition<S1> directCondition;
+  final BlocBinderCondition<S2> reverseCondition;
   BlocBinder({
     Key key,
     this.direct,
     this.reverse,
+    this.directCondition,
+    this.reverseCondition,
     Widget child,
   }) : super(child: child, key: key);
 
@@ -25,11 +31,13 @@ class BlocBinder<B1 extends Bloc<dynamic, S1>, S1, B2 extends Bloc<dynamic, S2>,
       listeners: [
         if (direct != null)
           BlocListener<B1, S1>(
+            condition: directCondition,
             listener: (context, state) =>
                 direct(context, state, context.bloc<B2>()),
           ),
         if (reverse != null)
           BlocListener<B2, S2>(
+            condition: reverseCondition,
             listener: (context, state) =>
                 reverse(context, state, context.bloc<B1>()),
           ),
