a0f1f144e95e4382bf59e1ced92df39918e484c8
Arseniy Kucher <orsen.kucher@gmail.com>
2020-11-28 21:24:52 +0200

    move to docker

--- main.go
+++ main.go
@@ -99,7 +99,7 @@ func StartServer(db *gorm.DB) {
 	http.HandleFunc("/adReport", s.AdReportHandler)
 	http.HandleFunc("/likeReport", s.LikeReportHandler)
 	hsrv := &http.Server{
-		Addr:    ":9091",
+		Addr:    ":9098",
 		Handler: nil, // use default mux
 	}
 	go func() {
