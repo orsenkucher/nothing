4c293abc3dd9fa62041417e4b8422017ae67edad
Arseniy Kucher <orsen.kucher@gmail.com>
2020-12-28 16:39:14 +0200

    wip: xp animation

--- frontend/nothing/lib/ui/label.dart
+++ frontend/nothing/lib/ui/label.dart
@@ -1,52 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:nothing/bloc/validation/bloc.dart';
 import 'package:nothing/bloc/xp/bloc.dart';
 import 'package:nothing/color/scheme.dart';
 import 'package:nothing/ui/xp.dart';
 
-class Label extends HookWidget {
+class Label extends StatelessWidget {
   const Label({
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final xpstate = useState(context.watch<XPBloc>().state);
-    useEffect(() {
-      final sub = context.watch<XPBloc>().asyncMap((state) {
-        xpstate.value = state;
-        print("HEH $state");
-        return Future.delayed(const Duration(milliseconds: 800));
-      }).listen((_) {});
-      return () => sub.cancel();
-    });
+    final bloc = context.watch<XPBloc>();
     final state = context.watch<ValidationBloc>().state;
     final child = state.when(
           just: (state) => state.maybeMap(
-            correct: (_) => _xp(context, xpstate.value),
+            correct: (_) => _xp(context, bloc),
             orElse: () => null,
           ),
           nothing: () => null,
         ) ??
         _title(context);
-    return BlocListener<XPBloc, XPState>(
-      listener: (context, state) {
-        // xpstate.value = state;
-        // print("XPBLOC2 $state");
-        // await Future.delayed(const Duration(milliseconds: 1800));
-      },
-      child: AnimatedSwitcher(
-        duration: const Duration(milliseconds: 220),
-        child: child,
-      ),
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 220),
+      child: child,
     );
   }
 
-  Widget _xp(BuildContext context, XPState state) {
+  Widget _xp(BuildContext context, XPBloc bloc) {
     return XP(
-      state: state,
+      bloc: bloc,
       sliderHeight: 8.0,
       sliderPadding: const EdgeInsets.symmetric(horizontal: 100.0),
     );
--- frontend/nothing/lib/ui/xp.dart
+++ frontend/nothing/lib/ui/xp.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:nothing/bloc/xp/bloc.dart';
 import 'package:nothing/color/scheme.dart';
@@ -5,12 +7,12 @@ import 'package:nothing/ui/slider.dart' as no;
 
 class XP extends StatefulWidget {
   const XP({
-    @required this.state,
+    @required this.bloc,
     @required this.sliderHeight,
     @required this.sliderPadding,
   });
 
-  final XPState state;
+  final XPBloc bloc;
   final double sliderHeight;
   final EdgeInsets sliderPadding;
 
@@ -21,12 +23,22 @@ class XP extends StatefulWidget {
 class _XPState extends State<XP> with SingleTickerProviderStateMixin {
   // AnimationController _controller;
   int phase = 0;
+  XPState state;
+  StreamSubscription _sub;
 
   @override
   void initState() {
     super.initState();
     // _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 220));
     // _controller.forward();
+
+    print('Subscribed!');
+    state = widget.bloc.state;
+    _sub = widget.bloc.listen((state) {
+      print('XP STATE $state');
+      state = state;
+    });
+
     phase = 0;
     () async {
       await Future.delayed(const Duration(milliseconds: 300));
@@ -36,34 +48,35 @@ class _XPState extends State<XP> with SingleTickerProviderStateMixin {
     }();
   }
 
-  @override
-  void didUpdateWidget(covariant XP oldWidget) {
-    if (oldWidget.state != widget.state) {
-      setState(() {
-        phase = 0;
-        () async {
-          await Future.delayed(const Duration(milliseconds: 300));
-          setState(() => phase = 1);
-          await Future.delayed(const Duration(milliseconds: 600));
-          setState(() => phase = 2);
-        }();
-      });
-    }
-    super.didUpdateWidget(oldWidget);
-  }
-
   // @override
-  // void dispose() {
-  //   _controller.dispose();
-  //   super.dispose();
+  // void didUpdateWidget(covariant XP oldWidget) {
+  //   if (oldWidget.bloc != widget.bloc) {
+  //     setState(() {
+  //       phase = 0;
+  //       () async {
+  //         await Future.delayed(const Duration(milliseconds: 300));
+  //         setState(() => phase = 1);
+  //         await Future.delayed(const Duration(milliseconds: 600));
+  //         setState(() => phase = 2);
+  //       }();
+  //     });
+  //   }
+  //   super.didUpdateWidget(oldWidget);
   // }
 
   @override
+  void dispose() {
+    // _controller.dispose();
+    _sub.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    var xp = widget.state.basexp;
-    if (xp == 0) xp = widget.state.bonusxp;
-    var next = widget.state.totalxp.toDouble() / widget.state.levelxp;
-    var prev = next - xp.toDouble() / widget.state.levelxp;
+    var xp = state.basexp;
+    if (xp == 0) xp = state.bonusxp;
+    var next = state.totalxp.toDouble() / state.levelxp;
+    var prev = next - xp.toDouble() / state.levelxp;
     if (phase == 0) {
       next = prev;
     }
@@ -84,7 +97,7 @@ class _XPState extends State<XP> with SingleTickerProviderStateMixin {
         alignment: Alignment.bottomCenter,
         child: AnimatedSwitcher(
           duration: const Duration(milliseconds: 220),
-          child: Text('+${xp}pt'),
+          child: Text('+${xp}pt\t lvl ${state.level}'),
         ),
       ),
     ]);
